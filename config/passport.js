98ece2315bb5b9df3c0692562931d1c6fdeac1c5
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-06-06 17:09:24 +0530

    added config file with logic for authentication of protected route

---
+++ config/passport.js
@@ -0,0 +1,26 @@
+const jwtStrtegy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.module('User');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const opts = {
+    jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+};
+
+module.exports = passport => {
+    passport.use(
+        new jwtStrtegy(opts, (jwt_payload, done) => {
+            User.findById(jwt_payload.user.id)
+            .then(user => {
+                if( user) {
+                    return done(null, user);
+                }
+                return done(null, false)
+            })
+            .catch (err => console.error(err));
+        })
+    );
+};
