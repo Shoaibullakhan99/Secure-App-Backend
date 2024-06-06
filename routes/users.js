816c119bb001c45638cb3242187704e40e26054a
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-06-06 16:59:54 +0530

    updated login route and logic with jwt authentication

--- routes/users.js
+++ routes/users.js
@@ -40,7 +40,16 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ msg: 'Invalid credentials' });
         }
         const isMatch = await bcrypt.compare(password, user.password);
-
+        if (isMatch){
+            return res.status(200).json({ msg: 'Invalid credentials' });
+        }
+        const payload = { user: {id: user.id} };
+        jwt.sign(
+            payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+                if(err) throw err;
+                res.status(200).json({ token })
+            }
+        );
     } catch (err){
         console.error(err.message);
         res.status(500).send('Server error')
