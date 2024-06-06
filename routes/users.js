8d6286bb6efc7e03891a17736f559043d4154c00
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-06-06 16:50:48 +0530

    added routes

---
+++ routes/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt.js');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const User = require('../models/User');
+
+
+// Load environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Register user
+router.post('register', async (req, res) => {
+    const { name, email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({ msg: 'User already exists'});
+        }
+        user = new User ({ name, email, password });
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        res.status(200).json({
+            msg: 'User registered successfully'
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
+
+// Authenticate user and get token
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne( { email });
+        if(!user){
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+    } catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+})
